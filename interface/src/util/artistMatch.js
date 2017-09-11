f9f3a67fff138e52da83e9714d575c25d71b5d1e
Evan Purkhiser <evanpurkhiser@gmail.com>
2017-09-11 00:38:29 -0500

    Search globally for artist splitters

--- interface/src/util/artistMatch.js
+++ interface/src/util/artistMatch.js
@@ -2,13 +2,13 @@
  * This is a fuzzy pattern used to split artists apart from their connectors.
  * This pattern does *not* strictly check for connectors in my defined format.
  */
-const splitOn = /(?:,| vs.?| &| and| f(?:ea)?t(?:uring)?\.?) /i;
+const splitOn = /(?:,| vs.?| &| and| f(?:ea)?t(?:uring)?\.?) /gi;
 
 /**
  * This is the strict version of the splitOn pattern that only matches the exact
  * connectors I've defined.
  */
-const strictSplitOn = /(?:,| vs| &| Ft\.) /;
+const strictSplitOn = /(?:,| vs| &| Ft\.) /g;
 
 /**
  * Split an artists string on common separators into a list of individual

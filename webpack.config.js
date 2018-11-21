bac54c33ad0e8ce69af8dac72cbcab20f62ac1f2
Evan Purkhiser <evanpurkhiser@gmail.com>
2018-11-21 00:28:17 -0800

    build: Bump to latest packages

--- webpack.config.js
+++ webpack.config.js
@@ -2,47 +2,65 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const commonChunk = new webpack.optimize.CommonsChunkPlugin({
-  name: 'vendor',
-  minChunks: module => /node_modules/.test(module.resource),
-});
+const IS_PROD =
+  process.argv.find(a => a.includes('mode=production')) !== undefined;
+
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: 'app/index.html',
+  }),
+  new webpack.DefinePlugin({
+    IS_PROD,
+    VERSION: JSON.stringify(process.env.VERSION || 'dev'),
+  }),
+  IS_PROD ? null : new webpack.HotModuleReplacementPlugin(),
+];
+
+const babelPlugins = [
+  ['@babel/plugin-proposal-decorators', { legacy: true }],
+  ['@babel/plugin-transform-react-display-name'],
+  ['@babel/plugin-proposal-export-default-from'],
+  ['@babel/plugin-proposal-class-properties'],
+  IS_PROD ? null : ['react-hot-loader/babel'],
+];
 
 module.exports = {
   entry: { app: './app/index.js' },
   output: {
     path: path.resolve(__dirname, '../build'),
-    filename: '[name].js',
+    filename: '[name].[hash].js',
   },
-  resolve: { alias: { app: path.resolve(__dirname, 'app') } },
-  devtool: 'source-map',
+  resolve: {
+    alias: { app: path.resolve(__dirname, 'app') },
+  },
+  devtool: IS_PROD ? 'source-map' : 'cheap-module-eval-source-map',
   devServer: { port: 9000, hot: true },
+  optimization: {
+    splitChunks: { chunks: 'all' },
+  },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
         loader: 'babel-loader',
-        query: { presets: ['env', 'stage-1', 'react'] },
+        options: {
+          presets: [
+            ['@babel/preset-env', { targets: { chrome: '64' } }],
+            ['@babel/preset-react'],
+          ],
+          plugins: babelPlugins.filter(x => x !== null),
+          compact: true,
+        },
       },
       {
         test: /\.scss$/,
-        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(eot|woff|woff2|ttf|svg|png|jpe?g|gif)(\?\S*)?$/,
-        loader: 'file-loader',
+        use: 'file-loader',
       },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({ template: 'app/index.html' }),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NamedModulesPlugin(),
-    commonChunk,
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'bundle',
-      minChunks: Infinity,
-    }),
-    new webpack.optimize.ModuleConcatenationPlugin(),
-  ],
+  plugins: plugins.filter(x => x !== null),
 };

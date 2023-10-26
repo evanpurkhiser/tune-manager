d2d5f1c905dfb10ed94476be62dc72755c79932d
Evan Purkhiser <evanpurkhiser@gmail.com>
2023-10-25 22:27:42 -0700

    update beatport script for new website

--- tune_manager/importer/beatport.py
+++ tune_manager/importer/beatport.py
@@ -1,3 +1,4 @@
+import json
 import bs4
 import requests
 from urllib.parse import urljoin
@@ -7,7 +8,13 @@ from tune_manager import mediafile
 
 # Beatports Genre mappings are pretty poor. For now, since I purchase *mostly
 # hardcore* remap a few genres automatically.
-genre_remapping = {"Hard Dance": "Hardcore", "Hardcore / Hard Techno": "Hardcore"}
+genre_remapping = {
+    "Hard Dance": "Hardcore",
+    "Hardcore / Hard Techno": "Hardcore",
+    "Trance (Main Floor)": "Trance",
+}
+
+user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
 
 
 def has_metadata(media):
@@ -30,29 +37,30 @@ def process(media):
     # Beatport seems to prefix the field with this control character
     track_url = media.mg_file.tags.get("WOAF").url.strip("\x03")
 
-    response = requests.get(track_url)
+    # Track details
+    response = requests.get(track_url, headers={"user-agent": user_agent})
 
     track = bs4.BeautifulSoup(response.content, "html.parser")
-    release_link = track.select("a.interior-track-release-artwork-link")[0]
-    release_url = urljoin("http://www.beatport.com", release_link["href"])
+    track_data = track.select("script#__NEXT_DATA__")[0]
+    track_data = json.loads(track_data.contents[0])
 
-    response = requests.get(release_url)
+    release_id = track_data["props"]["pageProps"]["track"]["release"]["id"]
+    release_url = urljoin("https://www.beatport.com/release/-/", str(release_id))
+
+    # Release details
+    response = requests.get(release_url, headers={"user-agent": user_agent})
+
     release = bs4.BeautifulSoup(response.content, "html.parser")
+    release_data = release.select("script#__NEXT_DATA__")[0]
+    release_data = json.loads(release_data.contents[0])
 
-    # Get the list of tracks
-    tracks = release.select("div.bucket.tracks ul.bucket-items li")
-
-    # Get a mapping of track details (under the artwork)
-    details = release.select("ul.interior-release-chart-content-list")[0]
-    details = details.select("li")
-
-    details = [i.find_all("span") for i in details]
-    details = {s[0].text.lower(): s[1].text.strip() for s in details}
+    catalog_id = release_data["props"]["pageProps"]["release"]["catalog_number"]
+    tracks = release_data["props"]["pageProps"]["release"]["tracks"]
 
     # Update the track, reporting only modified fields.
     orig = mediafile.serialize(media)
 
-    media.release = details["catalog"]
+    media.release = catalog_id
 
     # Track is a single
     if len(tracks) == 1:
